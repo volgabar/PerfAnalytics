0f1b6e09f68cf95f4735e17e9d31c702c5ebf825
Volga Barış Civelek <volgabarcivelek@gmail.com>
2020-11-20 20:24:27 +0300

    api_url fixed

--- dashboard/src/services/constants.js
+++ dashboard/src/services/constants.js
@@ -1,6 +1,6 @@
 const API_URL = {
     development: 'http://localhost:3000',
-    production: 'http://localhost:8080',
+    production: 'https://perf-analytics-app-api.herokuapp.com',
 };
 
 export const API_BASEURL = process.env.NODE_ENV === 'development' ? API_URL.development : API_URL.production;
